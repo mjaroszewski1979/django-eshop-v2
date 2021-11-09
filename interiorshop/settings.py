9e35119874d4ab62eb018086a60ba25a1b800f70
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2021-11-09 20:23:41 +0100

    Update settings.py

--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import cloudinary
+import cloudinary_storage
 from pathlib import Path
 import os
 

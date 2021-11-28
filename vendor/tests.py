e081ae51ab24f942bfe5f00cc2a084ff05d628b2
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2021-11-28 21:59:42 +0100

    Update tests.py

--- vendor/tests.py
+++ vendor/tests.py
@@ -1,26 +1,46 @@
 from django.test import TestCase, Client
 from vendor.models import Vendor
-from django.contrib.auth.models import User
 from django.urls import reverse, resolve
-from .views import become_vendor, vendor_admin, edit_vendor, vendors, vendor
+from .views import become_vendor, vendor_admin, add_product, edit_vendor, vendors, vendor
 from django.contrib.auth.forms import UserCreationForm
+from .forms import ProductForm
+from product.models import Product, Category
+import io
+from PIL import Image
+from django.utils.text import slugify
 
-
+def generate_photo_file():
+        file = io.BytesIO()
+        image = Image.new('RGBA', size=(100, 100), color=(155, 0, 0))
+        image.save(file, 'png')
+        file.name = 'test.png'
+        file.seek(0)
+        return file
 
 
 class VendorTest(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.data={
+        self.credentials={
             'username' : 'maciej',
             'password1' : 'jaroszewski123',
             'password2' : 'jaroszewski123'
         }
-        self.form = UserCreationForm(self.data)
+        self.form = UserCreationForm(self.credentials)
         self.user = self.form.save()
         self.new_vendor = Vendor.objects.create(name=self.user.username, created_by=self.user)
         self.pk=self.new_vendor.id
+        self.new_category = Category.objects.create(title='shoes', slug='shoes')
+        self.photo_file = generate_photo_file()
+        self.data = {
+                'category': self.new_category,
+                'image': self.photo_file,
+                'title': 'nike',
+                'description': 'nike',
+                'price': 20
+            }
+        self.client.login(username='maciej', password='jaroszewski123')
 
     def test_form_valid_data(self):
         self.assertTrue(self.form.is_valid())
@@ -82,7 +102,6 @@ class VendorTest(TestCase):
         self.assertEquals(resolve(url).func, vendor_admin)
 
     def test_vendor_admin_view(self):
-        self.client.login(username='maciej', password='jaroszewski123')
         response = self.client.get(reverse('vendor_admin'))
         self.assertContains(response, 'Vendor admin | URBAN STYLE', status_code=200)
         self.assertTemplateUsed(response, 'vendor/vendor_admin.html')
@@ -94,7 +113,6 @@ class VendorTest(TestCase):
         self.assertEquals(resolve(url).func, edit_vendor)
 
     def test_edit_vendor_get(self):
-        self.client.login(username='maciej', password='jaroszewski123')
         response = self.client.get(reverse('edit_vendor'))
         self.assertContains(response, 'Edit vendor | URBAN STYLE', status_code=200)
         self.assertTemplateUsed(response, 'vendor/edit_vendor.html')
@@ -102,7 +120,6 @@ class VendorTest(TestCase):
         self.assertEquals(type(response.context['vendor']), type(self.new_vendor))
 
     def test_edit_vendor_post(self):
-        self.client.login(username='maciej', password='jaroszewski123')
         data = {
             'name': 'maciej_new',
             'email': 'maciej@gmail.com'
@@ -110,3 +127,37 @@ class VendorTest(TestCase):
         response = self.client.post(reverse('edit_vendor'), data, follow=True)
         self.assertContains(response, 'Vendor admin | URBAN STYLE', status_code=200)
         self.assertTemplateUsed(response, 'vendor/vendor_admin.html')
+
+    def test_add_product_url_is_resolved(self):
+        url = reverse('add_product')
+        self.assertEquals(resolve(url).func, add_product)
+
+    def test_add_product_get(self):
+        form = ProductForm()
+        response = self.client.get(reverse('add_product'))
+        self.assertContains(response, 'Add product | URBAN STYLE', status_code=200)
+        self.assertTemplateUsed(response, 'vendor/add_product.html')
+        self.assertIsNotNone(response.context['form'])
+        self.assertEquals(type(response.context['form']), type(form))
+
+    def test_add_product_post(self):
+        response = self.client.post(reverse('add_product'), self.data, format='multipart')
+        self.assertEquals(response.status_code, 200)
+
+    def test_add_product_form(self):
+        product_form = ProductForm(self.data)
+
+        if product_form.is_valid():
+            product = product_form.save(commit=False)
+            product.vendor = self.user.vendor
+            product.slug = slugify(product.title)
+            product.save()
+            products = Product.objects.all()
+
+            self.assertIsNotNone(product)
+            self.assertEquals(product.title, 'nike')
+            self.assertEquals(product.slug, 'nike')
+            self.assertEquals(product.price, 20)
+            self.assertEquals(str(product.category), 'shoes')
+            self.assertEquals(str(product.vendor), 'maciej')
+            self.assertEquals(products.count(), 1)

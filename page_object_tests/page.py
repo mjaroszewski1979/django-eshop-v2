ffd985fd1fa0553a597b856da48fd7bf103f93ba
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2021-12-07 21:46:01 +0100

    Update page.py

--- page_object_tests/page.py
+++ page_object_tests/page.py
@@ -1,10 +1,7 @@
-from selenium_tests.element import BasePageElement
-from selenium_tests.locators import MainPageLocators
+from selenium.webdriver.support.ui import WebDriverWait as W
+from selenium.webdriver.support import expected_conditions as EC
+from selenium_tests.locators import LoginPageLocators, LogoutPageLocators, MainPageLocators, VendorAdmiPageLocators
 from selenium_tests.locators import SearchPageLocators
-
-class SearchTextElement(BasePageElement):
-
-    locator = 'button-search'
 
 
 class BasePage(object):
@@ -13,76 +10,81 @@ class BasePage(object):
     def __init__(self, driver):
         self.driver = driver
 
+    def do_click(self, locator):
+        W(self.driver, 10).until(EC.visibility_of_element_located(locator)).click()
+
+    def do_send_keys(self, locator, text):
+        W(self.driver, 10).until(EC.visibility_of_element_located(locator)).send_keys(text)
+
+    def get_element_text(self, locator):
+        element = W(self.driver, 10).until(EC.visibility_of_element_located(locator))
+        return element.text
+
+    def execute_login(self, username, password):
+        self.do_send_keys(LoginPageLocators.USERNAME, username)
+        self.do_send_keys(LoginPageLocators.PASSWORD, password)
+        self.do_click(LoginPageLocators.LOGIN_BUTTON)
+
+
 
 class MainPage(BasePage):
 
-    search_text_element = SearchTextElement()
 
     def is_title_matches(self):
-
         return "Welcome | URBAN STYLE" in self.driver.title
 
     def click_view_button(self):
-
-        element = self.driver.find_element(*MainPageLocators.VIEW_BUTTON)
-        element.click()
+        self.do_click(MainPageLocators.VIEW_BUTTON)
 
     def is_view_button_works(self, title):
-
         return title in self.driver.title
 
     def click_category_link(self):
-
-        element = self.driver.find_element(*MainPageLocators.CATEGORY_ITEM)
-        element.click()
+        self.do_click(MainPageLocators.CATEGORY_ITEM)
 
     def is_category_link_works(self, url):
-
         return url == self.driver.current_url
 
     def click_contact_link(self):
-
-        element = self.driver.find_element(*MainPageLocators.CONTACT_ITEM)
-        element.click()
+        self.do_click(MainPageLocators.CONTACT_ITEM)
 
     def is_contact_link_works(self, url):
-
         return url == self.driver.current_url
 
     def click_become_vendor_link(self):
-
-        element = self.driver.find_element(*MainPageLocators.BECOME_VENDOR)
-        element.click()
+        self.do_click(MainPageLocators.BECOME_VENDOR)
 
     def is_become_vendor_link_works(self, url):
-
         return url == self.driver.current_url
 
     def click_vendors_link(self):
-
-        element = self.driver.find_element(*MainPageLocators.VENDORS)
-        element.click()
+        self.do_click(MainPageLocators.VENDORS)
 
     def is_vendors_link_works(self, url):
-
         return url == self.driver.current_url
 
     def execute_search(self):
-        search_box = self.driver.find_element(*MainPageLocators.SEARCH_BOX)
-        search_button = self.driver.find_element(*MainPageLocators.SEARCH_BUTTON)
-        search_box.send_keys('red')
-        search_button.click()
+        self.do_send_keys(MainPageLocators.SEARCH_BOX, 'red')
+        self.do_click(MainPageLocators.SEARCH_BUTTON)
 
 
-class SearchResultsPage(BasePage):
+class SearchPage(BasePage):
 
 
     def is_results_found(self):
-        search_term = self.driver.find_element(*SearchPageLocators.SEARCH_TERM)
-        return 'SEARCH TERM: RED' in search_term.text
+        search_term = self.get_element_text(SearchPageLocators.SEARCH_TERM)
+        return 'SEARCH TERM: RED' in search_term
 
+class LoginPage(BasePage):
 
+    def is_login_works(self):
+        vendor_admin = self.get_element_text(VendorAdmiPageLocators.VENDOR_ADMIN)
+        return vendor_admin == 'VENDOR ADMIN |'
 
+class LogoutPage(BasePage):
 
+    def logout(self):
+        self.do_click(LogoutPageLocators.LOGOUT)
 
-
+    def is_logout_works(self, title):
+        return 'Welcome | URBAN STYLE' == title
